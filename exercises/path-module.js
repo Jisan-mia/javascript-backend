5908f7c882494a58b4e32fe8972f735858cfcf54
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-10-11 22:13:20 +0600

    node path module

---
+++ exercises/path-module.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const myPath = 'G:/1-development/backend/backend-javascript-lws/exercises/path-module.js';
+console.log(path.basename(myPath));
+
+console.log(path.dirname(myPath));
+
+console.log(path.extname(myPath));
+
+console.log(path.parse(myPath));
